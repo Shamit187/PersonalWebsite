6701cef492053fc14e0bd88e5f5d1f2788db808c
shamit187 <shamit187@gmail.com>
2025-02-28 18:14:38 +0600

    Forgot to delete past folders

--- frontend/src/app/map/Primes/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { TopicGraph } from "../TopicGraph";
-import { nodes, links } from "./data";
-
-export default function Home() {
-    return <TopicGraph nodeList={nodes} edgeList={links} />;
-}
